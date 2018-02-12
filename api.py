0ba81166920d294daea0c21ebba767aa34cd6883
Karl Ágústsson <kag@tskoli.is>
2018-02-12 14:30:37 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from bottle import route, run, template
+import os
 import requests
 #import json
 
@@ -12,4 +13,4 @@ def index():
     return template('index', data=data)
 
 
-run()
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
