4c5da795a617389c0dacca84efdd7fb2d643055c
Karl Ágústsson <kag@tskoli.is>
2018-02-15 13:37:29 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template
+from bottle import route, run, template, static_file
 import os
 import requests
 #import json
@@ -11,6 +11,10 @@ data = response.json()
 @route('/')
 def index():
     return template('index', data=data)
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./')
 
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
